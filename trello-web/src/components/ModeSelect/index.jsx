54603c593de67503a08644c6291bbfd969845e2c
AnhHoang <anhhoang01672966159@gmail.com>
2024-11-04 09:40:35 +0700

    cau hinh duong dan thu muc

--- trello-web/src/components/ModeSelect/index.jsx
+++ trello-web/src/components/ModeSelect/index.jsx
@@ -9,43 +9,43 @@ import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
 import Box from '@mui/material/Box';
 
 function ModeSelect() {
-    const { mode, setMode } = useColorScheme();
-  
-    const handleChange = (event) => {
-      setMode(event.target.value);
-    };
-  
-    return (
-      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-        <InputLabel id="mode-light-dark-system">Mode</InputLabel>
-        <Select
-          labelId="mode-light-dark-system"
-          id="mode-light-dark"
-          value={mode}
-          label="mode"
-          onChange={handleChange}
-        >
-          <MenuItem value="system"> 
-              <Box sx = {{display: 'flex', alignItems: 'center', gap: 1}}>
-                <SettingsBrightnessIcon fontSize='small'/> System
-              </Box>
-            </MenuItem>
-  
-            <MenuItem value="light">
-              <Box sx = {{display: 'flex', alignItems: 'center', gap: 1}}>
-                <LightModeIcon fontSize='small'/> Light
-              </Box> 
-            </MenuItem>
-  
-            <MenuItem value="dark"> 
-              <Box sx = {{display: 'flex', alignItems: 'center', gap: 1}}>
-                <DarkModeIcon fontSize='small'/> Dark
-              </Box>
-            </MenuItem>
-  
-        </Select>
-      </FormControl>
-    );
-  }
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    setMode(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="mode-light-dark-system">Mode</InputLabel>
+      <Select
+        labelId="mode-light-dark-system"
+        id="mode-light-dark"
+        value={mode}
+        label="mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </Box>
+        </MenuItem>
+
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+
+      </Select>
+    </FormControl>
+  );
+}
 
 export default ModeSelect
