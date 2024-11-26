47de0853f5a878b1dd0f05794d183c6a36b129d4
Ngô Đức Quang Anh <ngoquanganhalb@gmail.com>
2024-11-27 06:04:15 +0700

    validation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes' // Correct import
-import { boardRoutes } from './boardRoutes'
+import { boardRoute } from './boardRoute'
 
 const Router = express.Router()
 //check APIs V1 status
@@ -8,6 +8,6 @@ Router.get('/status', (req, res) => {
   res.status(StatusCodes.OK).json({ message: 'API v1 is ready to use',code:StatusCodes.OK })
 })
 //Board APIs
-Router.use('/boards', boardRoutes)
+Router.use('/boards', boardRoute)
 
 export const APIs_V1 = Router
---
+++ src/validations/boardValidation.js
@@ -0,0 +1,37 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => { 
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required':'Title is required',
+      'string.empty':'Title is not allowed to be empty',
+      'string.min':'Title min 3 chars',
+      'string.max':'Title max 50 chars',
+      'string.trim':'Title must not have leading or trailing whitespace'
+    }), // string,batbuoc/soluongkitumin/max
+    description: Joi.string().required().min(3).max(256).trim().strict()
+
+  })
+
+  try {
+    // console.log('req.body:',req.body)
+
+    await correctCondition.validateAsync(req.body,{abortEarly:false}) //kiem tra req.body tu fe gui len co dung voi DK cua doi tuong correctCondition ko
+    
+    // next()//dua req sang tang xu ly khac 
+    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: API create new boards' })
+
+  } catch (error) {
+    // console.log(error)
+    // console.log(new Error(error))
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ ////code : 422 validate data
+      errors: new Error(error).message
+    })
+    
+  }
+
+}
+export const boardValidation = {
+  createNew
+}
