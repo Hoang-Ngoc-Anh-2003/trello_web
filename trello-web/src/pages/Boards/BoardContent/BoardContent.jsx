d97580ff4e06b1779abb319a2ba14b22a5da171f
AnhHoang <anhhoang01672966159@gmail.com>
2024-12-02 07:48:24 +0700

    keo tha columns

--- trello-web/src/pages/Boards/BoardContent/BoardContent.jsx
+++ trello-web/src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,21 +1,79 @@
-import Box from "@mui/material/Box"
-import ListColumns from "./ListColumns/ListColumns"
-import { mapOrder } from "~/utils/sorts"
+import Box from "@mui/material/Box";
+import ListColumns from "./ListColumns/ListColumns";
+import { mapOrder } from "~/utils/sorts";
 
-function BoardContent({board}) {
-     const orderedColumns = mapOrder (board?.columns, board?.columnOrderIds, '_id')
-    return (
-        <Box sx={{
-                bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495E' : '#1976D2'),
-                width: '100%',
-                display: 'flex',
-                flexGrow: 1,
-                height: (theme) => theme.trello.boardContentHeight,
-                p: '10px 0'
-            }}>
-            <ListColumns columns={orderedColumns}/> 
-        </Box>
-    )
+import {
+  DndContext,
+  PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+import { useEffect, useState } from "react";
+
+function BoardContent({ board }) {
+  // yeu cau chuot di chuyen 10px thi moi goi even keo tha, fix truong hop click goi even
+  // const pointerSensor = useSensor(PointerSensor, {
+  //   activationConstraint: { distance: 10 },
+  // });
+
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 10 },
+  });
+  // nhan giu 250ms va dung sai cam ung(lien quan den phan dung but vs tay keo tren mobile) thi moi kich hoat event
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: { delay: 250, tolerance: 500 },
+  });
+  // const mySensors = useSensors(pointerSensor);
+  const mySensors = useSensors(mouseSensor, touchSensor);
+  const [orderedColumns, setOrderedColumns] = useState([]);
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
+  }, [board]);
+
+  const handleDragEnd = event => {
+    console.log("handleDragEnd: ", event);
+    const { active, over } = event;
+    // kiem tra neu khong ton tai over ( keo tra ngoai thi return luon)
+    if (!over) return;
+    // neu vi tri sau khi keo tha khac voi vi tri ban dau
+    if (active.id !== over.id) {
+      // lay vi tri cu ( tu active)
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id);
+      // lay vi tri moi (tu over)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id);
+      // dung arrayMove cua dnd_kit de sap xep lai mang Columns ban dau
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex);
+      // 2 console.log du lieu nay dung de xu ly goi API
+      // const dndOderedColumnsIds = dndOrderedColumns.map(c => c._id);
+      // console.log("dndOderedColumns: ", dndOrderedColumns);
+      // console.log("dndOderedColumnsIds: ", dndOderedColumnsIds);
+
+      // cap nhat lai state columns ban dau sau khi keo tha
+      setOrderedColumns(dndOrderedColumns);
+    }
+  };
+
+  return (
+    <DndContext onDragEnd={handleDragEnd} sensors={mySensors}>
+      <Box
+        sx={{
+          bgcolor: theme =>
+            theme.palette.mode === "dark" ? "#34495E" : "#1976D2",
+          width: "100%",
+          display: "flex",
+          flexGrow: 1,
+          height: theme => theme.trello.boardContentHeight,
+          p: "10px 0",
+        }}
+      >
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
+  );
 }
 
-export default BoardContent
+export default BoardContent;
