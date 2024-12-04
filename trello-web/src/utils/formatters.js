c8887b784b877591deccc0f8611986867e8405e0
AnhHoang <anhhoang01672966159@gmail.com>
2024-12-05 00:17:17 +0700

    fix không kéo được card vào column rỗng

--- trello-web/src/utils/formatters.js
+++ trello-web/src/utils/formatters.js
@@ -4,22 +4,27 @@
 /**
  * Capitalize the first letter of a string
  */
-export const capitalizeFirstLetter = (val) => {
-    if (!val) return ''
-    return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
-  }
-  
-  /**
-   * Example:
-   */
+export const capitalizeFirstLetter = val => {
+  if (!val) return "";
+  return `${val.charAt(0).toUpperCase()}${val.slice(1)}`;
+};
+
+/**
+ * Example:
+ */
 //   const stringTest = 'trungquandev'
 //   const capString = capitalizeFirstLetter(stringTest)
-  
+
 //   console.log('stringTest:', stringTest)
 //   console.log('capString:', capString)
-  /**
-   * Results:
-   * 
-   * stringTest: Of course, nothing changes =))
-   * capString: Trungquandev
-   */
+
+// --------------------------------------------------------------
+
+export const generatePlaceholderCar = column => {
+  return {
+    _id: `${column._id}-placeholder-card`,
+    boardId: column.boardId,
+    columnId: column._id,
+    FE_PlaceholderCard: true,
+  };
+};
