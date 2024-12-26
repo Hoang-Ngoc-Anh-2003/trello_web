c05b5ce3f64d9bf2ea409db5386d4661b6784f80
ngoquanganhalb <ngoquanganhalb@gmail.com>
2024-12-26 21:26:18 +0700

    save changes before switch to main fe

--- trello-web/src/utils/constants.js
+++ trello-web/src/utils/constants.js
@@ -1,2 +1,3 @@
 // bien goi API, chua bien tinh, hang so
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
+export const API_ROOT = 'https://trello-api-private.onrender.com'
